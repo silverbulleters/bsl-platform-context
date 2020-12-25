a8ed9584f20bcbac792cfe7569d78c312018b3da
Tymko Oleg <olegtymko@yandex.ru>
2020-12-25 13:22:10 +0700

    #1 / Заготовка проекта

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import java.net.URI
+
+group = "org.github.silverbulleters"
+version = gitVersionCalculator.calculateVersion("v")
+var javaVersion = JavaVersion.VERSION_11
+var encoding = "UTF-8"
+val junitVersion = "5.6.1"
+
+plugins {
+    java
+    id("io.franzbecker.gradle-lombok") version "4.0.0"
+    id("com.github.gradle-git-version-calculator") version "1.1.0"
+    id("net.kyori.indra.license-header") version "1.2.1"
+}
+
+repositories {
+    mavenCentral()
+    maven { url = URI("https://jitpack.io") }
+}
+
+java {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
+
+dependencies {
+    compileOnly("org.projectlombok", "lombok", lombok.version)
+
+    // junit
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", junitVersion)
+    testImplementation("org.junit.jupiter", "junit-jupiter-params", junitVersion)
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = encoding
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed", "standard_error")
+    }
+
+    reports {
+        html.isEnabled = true
+    }
+}
+
+lombok {
+    version = "1.18.16"
+}
+
+license {
+    header = rootProject.file("HEADER.txt")
+    ext["year"] = "2020"
+    ext["name"] = "Team SilverBulleters <team@silverbulleters.org> and contributors"
+    ext["project"] = "BSL platform context"
+    exclude("**/*.properties")
+    exclude("**/*.xml")
+    exclude("**/*.json")
+    exclude("**/*.txt")
+    exclude("**/*.java.orig")
+    exclude("**/*.impl")
+}
---
+++ src/main/java/org/silverbulleters/bsl/platform/context/BSLPlatformContext.java
@@ -0,0 +1,25 @@
+/*
+ * This file is a part of BSL platform context.
+ *
+ * Copyright © 2020
+ * Team SilverBulleters <team@silverbulleters.org> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL platform context is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL platform context is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL platform context.
+ */
+package org.silverbulleters.bsl.platform.context;
+
+public class BSLPlatformContext {
+}
